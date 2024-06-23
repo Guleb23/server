6ff032884bad5922eeeb0fa0e81dc35b2615376a
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2024-06-24 01:37:49 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const OpenAI = require("openai");
+const bodyParser = require("body-parser");
+require('dotenv').config()
+const PORT = 4000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/api/suggestions", async (req, res) => {
+    const OPENAI_API_KEY = process.env.API_KEY;
+    const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+    const aiModel = "gpt-3.5-turbo";
+      const inpJson = req.query.inputs;
+      if(inpJson && inpJson.length){
+          const prompt = []
+          prompt.push("you are expert in recomending music");
+          prompt.push("Based on the following inputs, suggest me a good song to listen");
+          prompt.push("Ответ нужен на русском языке");
+          prompt.push(inpJson);
+          const messages = [{
+              role:"system",
+              content : prompt.join(' '),
+          }];
+          const completion = await openai.chat.completions.create({
+              model : aiModel,
+              messages,
+              
+          });
+          const aiResponse = completion.choices[0].message.content;
+          res.setHeader('Content-Type', 'application/json');
+          
+          res.json({aiResponse})
+      }else{
+          res.json({message: "sssss"})
+      }
+  })
+
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
